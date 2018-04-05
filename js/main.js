12f404c91f63eb0ff2a04f265f3cce6292514b72
Blake Shaw <blake.m.shaw@gmail.com>
2018-04-05 15:03:31 -0400

    latest and greatest

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 new Vue({
-  el: '#example',
+  el: '#pictures',
   data: {
     slides: 7
   },
@@ -8,3 +8,15 @@ new Vue({
     'slide': Carousel3d.Slide
   }
 })
+
+$(".nav-link").on('click',function(e) {
+   e.preventDefault(); // stops link from loading
+   $('.content').hide(); // hides all content divs
+   $($(this).attr('href')).show(); //get the href and use it find which div to show
+});
+
+$(".slide").on('click',function(e) {
+   e.preventDefault(); // stops link from loading
+   $('.caption').hide(); // hides all content divs
+   $('#' + $(this).attr('id') + "Caption").show(); //get the href and use it find which div to show
+});
