33b1fcfc1b55b68d919531d116e0f7fca558e3b3
Shaw <blakeshaw@quickenloans.com>
2018-04-08 22:15:40 -0400

    background, smaller, other stuff

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 new Vue({
   el: '#pictures',
   data: {
-    slides: 7
+    slides: 7,
   },
   components: {
     'carousel-3d': Carousel3d.Carousel3d,
@@ -10,13 +10,26 @@ new Vue({
 })
 
 $(".nav-link").on('click',function(e) {
-   e.preventDefault(); // stops link from loading
-   $('.content').hide(); // hides all content divs
-   $($(this).attr('href')).show(); //get the href and use it find which div to show
+  $('.content').hide(); // hides all content divs
+  $($(this).attr('href')).show(); //get the href and use it find which div to show
+});
+
+$("#homeLink").on('click',function(e) {
+  $('.content').hide(); // hides all content divs
+  $($(this).attr('href')).show(); //get the href and use it find which div to show
 });
 
 $(".slide").on('click',function(e) {
-   e.preventDefault(); // stops link from loading
-   $('.caption').hide(); // hides all content divs
-   $('#' + $(this).attr('id') + "Caption").show(); //get the href and use it find which div to show
+  $('.caption').hide(); // hides all caption divs
+  $('#' + $(this).attr('id') + "Caption").show(); //get the slide number and use it find which div to show
+});
+
+$(function() {
+  $('.content').hide(); // hides all content divs
+  $(window.location.hash).show(); //get the href and use it find which div to show
+  $('.caption').hide(); // hides all caption divs
+  $('#slide1Caption').show(); //get the slide number and use it find which div to show
+  if(window.location.hash==""){
+    $('#home').show();
+  }
 });
